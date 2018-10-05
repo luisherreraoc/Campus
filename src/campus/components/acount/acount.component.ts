d446edcb0229c2b747c211d349f946cb3d5d6369
Luis Acedo <lacedo@oceano.com>
2018-10-05 13:30:54 +0200

    se muestra campo especializacion de usuario, mail y dni, añadido boton actualizar para abrir pop up de especializaciones y boton aceptar para actualizar los datos desde pagina cuenta

--- src/campus/components/acount/acount.component.ts
+++ src/campus/components/acount/acount.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ViewContainerRef, ViewChild, ElementRef } from '@angular/core';
+import { Component, Inject, ViewContainerRef, ViewChild, ElementRef, SimpleChange, SimpleChanges } from '@angular/core';
 import { Observable, BehaviorSubject, Subscription } from "rxjs/Rx";
 
 import { environment } from '../../../environments/environment';
@@ -16,6 +16,7 @@ import { User } from '../../models/user.model';
 
 import { Loader } from 'mk';
 import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Component({
 	templateUrl: './acount.component.html',
@@ -26,10 +27,12 @@ export class AcountComponent
 	@ViewChild('button') private _button: ElementRef;
 
 	private _form: MkForm;
+	private _form_group: FormGroup;
 	private _subscriptions: Array<Subscription>;
 
 	private _onl1: Array<string>;
 	private _onl2: Array<string>;
+	private _onl3: Array<string>;
 	private _key: string;
 
 	private _ids: any;
@@ -39,6 +42,8 @@ export class AcountComponent
 	private _response_obj: {title:string,text:string,img:string,btn:string,callback:any};
 	private _sent: boolean;
 
+	private _user : any;
+	private julome : Array<any>;
 	// private showMe: boolean;
 
 	public constructor ( 
@@ -55,6 +60,7 @@ export class AcountComponent
 
 		this._onl1 = ['oauth_user_first_name', 'oauth_user_last_name'];
 		this._onl2 = ['user_details_prefix', 'oauth_user_phone'];
+		this._onl3 = ['oauth_user_dni', 'oauth_user_email'];
 	 	this._key = environment.icon_key;
 	 	this._ids = null;
 
@@ -69,25 +75,31 @@ export class AcountComponent
             callback: null
 		};
 		this._sent = false;
+
+		this.julome = [];
 	}
 
 	public ngOnInit () : void
 	{
 		let source: User = this._us.users.getValue().find( (usr:User) => usr['token'] === this._as.getToken() );
 		let obs: Observable<any> = source ? Observable.of(source) : this._us.get(this._as.getToken());
-
+		
 		this._loader.show('acount');
 
-		this._subscriptions = [	this.subscriptions(obs) ];
-	}	
+		this._subscriptions = [	this.subscriptions(obs), this.getUserDetails(obs) ];
+	}
+
+	public ngOnChanges (changes: SimpleChanges) : void {
+		console.log('something happened')
+	}
 
 	public ngOnDestroy () : void 
     { 
         this._subscriptions.forEach( sub => sub.unsubscribe() );
         this._subscriptions.length = 0;
 		this._loader.dismiss('acount'); 
-    }
-
+	}
+	
     private subscriptions ( observable: Observable<any> ) : Subscription
     {
     	return observable
@@ -96,20 +108,54 @@ export class AcountComponent
     		if ( user.oauth_user_id === undefined ) {
     			this._as.logout();
 			}
-    		this._ids = {'user': user.oauth_user_id }; 
+			this._ids = {'user': user.oauth_user_id }; 
     		return this._fs.forms.map( forms => forms.find( form => form.name === "user" ));
     	})
     	.subscribe( form =>
         {
         	if (form) 
             { 	
-            	this._form = form;
+				this._form = form;
+				this._form_group = this._form.formGroup;
 				this._loader.dismiss('acount');
 			}
         },
         (error) => {},
         () => this._loader.dismiss('acount'));
 	}
+
+	private getUserDetails (obs : Observable<any>) : Subscription {
+		
+		return obs.subscribe( user => {
+			this._us.getById(user.oauth_user_id)
+			.subscribe((user :any) => {
+				this._user = JSON.parse(user._body);
+
+				let user_details = this._user.data.oauth_user_details;
+
+				let pitart = [];
+
+				for (let seak in user_details) {
+					if(user_details[seak].user_details_key !== 'prefix')
+					{
+						pitart.push(user_details[seak].user_details_value);
+					}
+				}
+
+				let renato = '';
+
+				for (let i=0; i < pitart.length; i++) {
+					if (i !== pitart.length -1 ) {
+						renato = pitart[i] + ' - ';
+					} else {
+						renato = pitart[i]
+					}
+					this.julome.push(renato);
+				}
+			})
+		})
+	}
+
 
 	private falseClick() {
         let clickMe = this._button.nativeElement;
@@ -153,5 +199,48 @@ export class AcountComponent
 
 	private goToAccount () : void {
 		this._sent = false;
+	}
+
+	private send () : void {
+		let aux = this._form_group.getRawValue();
+            
+		let data = {
+			'first_name': aux.oauth_user_first_name,
+			'last_name': aux.oauth_user_last_name,
+			'phone': aux.oauth_user_phone
+		}
+		this._us.update(data)
+        .subscribe( (response: any ) =>
+        {
+            let user : any = this._us.data.find((user : any) => {
+                return user.oauth_user_id == this._ids.user
+            })
+            
+            user.oauth_user_first_name = aux.oauth_user_first_name;
+            user.oauth_user_last_name = aux.oauth_user_last_name;
+            user.oauth_user_phone = aux.oath_user_phone;
+            user.user_details_prefix = aux.user_details_prefix;
+		},
+		( error : any ) => {
+			this._sent = true;
+			this._response_obj = {
+				title: '',
+				text: 'Ha habido un error a la hora de actualizar sus datos. Por favor, vuelva a intentarlo',
+				img: '',
+				btn: 'VOLVER',
+				callback: this.goToAccount
+			}
+        },
+        () => {
+			this._sent = true;
+			this._response_obj = {
+				title: '',
+				text: 'Sus datos se han actualizado correctamente',
+				img: '',
+				btn: 'VOLVER',
+				callback: this.goToAccount
+			}
+        }
+        );
 	}
 }
