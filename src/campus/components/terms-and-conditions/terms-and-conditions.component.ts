550dbfce964d3a4717c987bf73223bb7faf9f096
Luis Acedo <lacedo@oceano.com>
2019-03-21 17:16:20 +0100

    creado component pte para pagina de terminos y condiciones

---
+++ src/campus/components/terms-and-conditions/terms-and-conditions.component.ts
@@ -0,0 +1,15 @@
+import { Component }                         	from '@angular/core';
+
+@Component({
+	templateUrl: './terms-and-conditions.component.html',
+	styleUrls: ['./terms-and-conditions.component.scss']
+})
+export class TermsAndConditionsComponent
+{
+
+	public constructor (  ) 
+	{ 
+		
+	}
+
+}
